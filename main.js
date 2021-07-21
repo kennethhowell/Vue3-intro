3a1ba5bbc82ae28c51b9f281feadbc897818f52f
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-21 11:38:45 -0500

    complete: lesson 6

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const app = Vue.createApp({
             description:  'Warm with breathable design to keep you from having those awful sweaty feets',
             image: './assets/images/socks_blue.jpg',
             url: "https://www.google.com",
-            inventory: 0,
+            inventory: 1,
             onSale: true,
             details: ['Free range', 'hormone free', 'low sodium'],
             variants: [
@@ -24,10 +24,12 @@ const app = Vue.createApp({
 
         methods:{
             addToCart(){
-                this.cart += 1
+                this.cart += 1,
+                this.inventory -= 1
             },
             removeFromCart(){
-                this.cart -= 1
+                this.cart -= 1,
+                this.inventory += 1
             },
             updateImage(variantImage) {
                 this.image = variantImage;
