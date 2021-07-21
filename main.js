a92dd7548596ab1a9692f638fb0231677ac15ab8
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-21 11:00:29 -0500

    complete: lesson 4

--- main.js
+++ main.js
@@ -4,7 +4,18 @@ const app = Vue.createApp({
             product: "Socks",
             description:  'Warm with breathable design to keep you from having those awful sweaty feets',
             image: './assets/images/socks_blue.jpg',
-            url: "https://www.google.com"
+            url: "https://www.google.com",
+            inventory: 0,
+            onSale: true,
+            details: ['Free range', 'hormone free', 'low sodium'],
+            variants: [
+                {id: 0330, color: 'green'},
+                {id: 0810, color: 'blue'}
+            ],
+            sizes: ['xiao', 'petite', 'average joe', 'bolder buddy', 'husky human', 'extra large twice in charge']
+
+
+
             // testMethod: function(){
             //     alert('Yo!')
             }
