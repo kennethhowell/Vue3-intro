b87276164ee4eacd9520f8eb4bdb07fbc0749cc6
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-21 10:20:13 -0500

    complete: lesson 1 from vue mastery

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function(){
+        return {
+            product: 'Socks',
+            description:  'Warm with breathable design to keep you from having those awful sweaty feets'
+            // testMethod: function(){
+            //     alert('Yo!')
+            }
+        }
+    }
+)
