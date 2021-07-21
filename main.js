af6b246eece6115c876571b708705d7f2cf90bd3
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-21 11:18:49 -0500

    complete: lesson 5

--- main.js
+++ main.js
@@ -9,15 +9,28 @@ const app = Vue.createApp({
             onSale: true,
             details: ['Free range', 'hormone free', 'low sodium'],
             variants: [
-                {id: 0330, color: 'green'},
-                {id: 0810, color: 'blue'}
+                {id: 0330, color: 'green', image: './assets/images/socks_green.jpg'},
+                {id: 0810, color: 'blue', image: './assets/images/socks_blue.jpg'}
             ],
-            sizes: ['xiao', 'petite', 'average joe', 'bolder buddy', 'husky human', 'extra large twice in charge']
+            sizes: ['xiao', 'petite', 'average joe', 'bolder buddy', 'husky human', 'extra large twice in charge'],
+            cart: 0
 
 
 
             // testMethod: function(){
             //     alert('Yo!')
+            }
+        },
+
+        methods:{
+            addToCart(){
+                this.cart += 1
+            },
+            removeFromCart(){
+                this.cart -= 1
+            },
+            updateImage(variantImage) {
+                this.image = variantImage;
             }
         }
     }
