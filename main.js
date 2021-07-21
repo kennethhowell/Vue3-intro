1e0e7ec0b33ed0b87902e6def119679cf11a99c2
kennethhowell <howell.kenneth.a@gmail.com>
2021-07-21 10:35:29 -0500

    complete: lesson 3

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const app = Vue.createApp({
     data: function(){
         return {
-            product: 'Socks',
-            description:  'Warm with breathable design to keep you from having those awful sweaty feets'
+            product: "Socks",
+            description:  'Warm with breathable design to keep you from having those awful sweaty feets',
+            image: './assets/images/socks_blue.jpg',
+            url: "https://www.google.com"
             // testMethod: function(){
             //     alert('Yo!')
             }
